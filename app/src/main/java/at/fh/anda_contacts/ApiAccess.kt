d3c9800d39f5986506347b7ce166def2fb1a8f40
eynimeni <maganatte@yahoo.de>
2022-10-19 11:08:44 +0200

    installation plugins etc complete

---
+++ app/src/main/java/at/fh/anda_contacts/ApiAccess.kt
@@ -0,0 +1,22 @@
+package at.fh.anda_contacts
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class ApiContact(val name: String, val telephoneNumber: Int, val age: Int)
+
+fun createHttpClient() = HttpClient(Android) {
+    expectSuccess = true
+    engine {
+        connectTimeout = 60000
+    }
+    install(ContentNegotiation) {
+        json(Json {
+            ignoreUnknownKeys = true
+        })
+    }
+}
